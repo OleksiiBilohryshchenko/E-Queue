2f308a919d9b5e9c9540eb6fba727b8933aacd5d
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-11-02 17:37:28 -0400

    Html and JS

--- src/main/resources/static/client.js
+++ src/main/resources/static/client.js
@@ -1,6 +1,24 @@
+
+var countdownInterval;
+
 // Fetch user info and update UI
 $.get("/user", function (data) {
     $("#user").html('Logged in as: ' + data.name);
     $(".unauthenticated").hide();
     $(".authenticated").show();
 });
+
+function logout() {
+    $.post("/logout", function () {
+        $("#user").html('');
+        $(".authenticated").hide();
+        $(".unauthenticated").show();
+
+        // Clear booking slots
+        $('#booking-slot').empty();
+        if (countdownInterval) {
+            clearInterval(countdownInterval);
+        }
+    });
+    return false;
+}
