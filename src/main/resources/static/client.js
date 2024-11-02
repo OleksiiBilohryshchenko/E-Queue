0f82bafbd239f90c69fd8dde5b25c2e1719dda01
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-11-02 17:42:23 -0400

    Html and JS

--- src/main/resources/static/client.js
+++ src/main/resources/static/client.js
@@ -6,6 +6,9 @@ $.get("/user", function (data) {
     $("#user").html('Logged in as: ' + data.name);
     $(".unauthenticated").hide();
     $(".authenticated").show();
+
+    loadBookingSlots();
+
 });
 
 function logout() {
@@ -21,4 +24,25 @@ function logout() {
         }
     });
     return false;
+}
+
+// Load booking slots into the dropdown
+function loadBookingSlots() {
+    $.get("/api/booking/slots", function (data) {
+        var $slotSelect = $('#booking-slot');
+        $slotSelect.empty();
+
+        console.log('Loading booking slots: ' + data.length);
+        if (data.length > 0) {
+            data.forEach(function (slot) {
+                $slotSelect.append('<option value="' + slot.id + '">' + slot.startTime + ' - ' + slot.endTime + '</option>');
+            });
+            $('#no-slots-message').hide();
+            $('#booking-form').show();
+        } else {
+            // No slots available
+            $('#booking-form').hide();
+            $('#no-slots-message').show().text('No slots available. Please try next day.');
+        }
+    });
 }
