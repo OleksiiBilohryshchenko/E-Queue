de5c59a553ef5e386a05255a8d73a29262650c36
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-11-02 20:13:32 -0400

    Docker update

---
+++ src/main/java/com/controller/BookingController.java
@@ -0,0 +1,29 @@
+package com.controller;
+
+import com.example.BookingSlot;
+import com.example.BookingSlotRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.StreamSupport;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class BookingController {
+
+    private final BookingSlotRepository bookingSlotRepository;
+
+    @GetMapping("/api/booking/slots")
+    public List<BookingSlot> getBookingSlots() {
+        return StreamSupport.stream(bookingSlotRepository.findAll().spliterator(),false)
+                .sorted(Comparator.comparing(BookingSlot::startTime))
+                .toList();
+
+    }
+
+}
